05793b2c5179d389f54057bb1aa62ba8a3fcbc86
Pau <pau@apl.cat>
2017-09-30 12:31:03 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-sed -i "s/app:x:1000/app:x:`id -u`/" /etc/passwd
 
+# Adjust user id (Useful if host assign random uid) 
+UID=${UID:-`id -u`}
+sed -i "s/app:x:[[:digit:]]/app:x:$UID/" /etc/passwd
+
+# Run SSH service
 ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa && \
 ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa && \
 ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa && \
